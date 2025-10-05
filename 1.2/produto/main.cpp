695c6afc74e6643347ec24d31372ee0e4854d6df
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-05 05:20:26 -0300

    :style: Atualiza o estilo do arquivo 1.2/produto/main.cpp

--- 1.2/produto/main.cpp
+++ 1.2/produto/main.cpp
@@ -21,5 +21,7 @@ int main()
     std::cout << Mul (2, 4, 5) << '\n';    // 40 (int)
     std::cout << Mul (2, 3.5) << '\n';     // 7.0 (promove para double)
     std::cout << Mul (2u, 3, 0.5f) << '\n'; // 3.0 (promove para float/double)
+    std::cout << Mul (2u, 3, 0.5f) << '\n'; // 3.0 (promove para float/double)
+    std::cout << Mul (3.17, 3.8, 0.5) << '\n'; // 3.0 (promove para float/double)
     return 0;
 }
